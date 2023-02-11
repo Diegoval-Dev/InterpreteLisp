6486324b4228dda0d3863ca3798b467959009516
Diego <diegopablo012@gmail.com>
2023-02-11 14:06:25 -0600

    Inicio model

---
+++ src/Model/Aritmetica.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Aritmetica {
+    Stack<Integer> stack = new Stack<Integer>();
+
+    public int Control(String expression){
+        int result = 0;
+
+        return result;
+    }
+    public String
+}
