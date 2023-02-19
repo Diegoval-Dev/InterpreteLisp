74dc19cc5e224091cf6cf9683c7952dd1408af57
Anaru03 <dele22428@uvg.edu.gt>
2023-02-18 22:01:47 -0600

    Intento no1. de verificacion de desarrollo si la sintaxis esta mala o buena

--- src/Model/Defensiva.java
+++ src/Model/Defensiva.java
@@ -1,4 +1,18 @@
 package Model;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Defensiva {
+    private static final Pattern PATRON_EXPRESION = Pattern.compile("^\\d+(\\s*[+\\-*/]\\s*\\d+)*$");
+
+    public static void validar(String expresion) throws IllegalArgumentException {
+        Matcher patrones = PATRON_EXPRESION.matcher(expresion);
+        if (!patrones.matches()) {
+            throw new IllegalArgumentException("La expresión aritmética está mal formada");
+        } else {
+            System.out.println("La expresión aritmética es válida");
+        }
+    }
+
+
 }
