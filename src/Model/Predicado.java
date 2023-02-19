d2684fdf2200c3776fe5f084c6c4e285020aae1e
Anaru03 <dele22428@uvg.edu.gt>
2023-02-18 21:58:34 -0600

    Pequeña modificación del predicado

--- src/Model/Predicado.java
+++ src/Model/Predicado.java
@@ -4,18 +4,25 @@ import java.util.ArrayList;
 
 public class Predicado {
     public boolean Atom(String x){
-        return false;
+        return false; //o true
     }
+
     public ArrayList<String> List(String x){
-        return new ArrayList<>();
+        ArrayList<String> listaArr = new ArrayList<String>();
+        listaArr.add(x);
+        return listaArr;
     }
+
     public boolean Equals(String x, String y){
-        return false;
+        return x.equals(y);
     }
+
     public boolean Menor(int n1, int n2){
-        return false;
+        return n1 < n2;
     }
+
     public boolean Mayor(int n1, int n2){
-        return false;
+        return n1 > n2;
     }
+
 }
