1f8a98367d4539159515ba3f5d1e427904b2dc10
Anaru03 <dele22428@uvg.edu.gt>
2023-02-20 09:34:43 -0600

    Inteno no.1 no terminado del unit test sobre expresiones regulares

--- src/Test/DefensivaTest.java
+++ src/Test/DefensivaTest.java
@@ -3,6 +3,8 @@ package Test;
 import Model.Defensiva;
 
 import org.junit.Test;
+import static Model.Defensiva.sinLetras;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class DefensivaTest {
@@ -13,6 +15,16 @@ public class DefensivaTest {
         assertTrue(result);
         System.out.println("La expresión '" + expression + "' es " + (result ? "válida" : "inválida"));
     }
+
+
+    @Test
+    public void testExpresionAritmeticaValida() {
+        String expression2 = "(+ (+1  5) 5)";
+        boolean result2 = Defensiva.sinLetras(expression2);
+        assertTrue(result2);
+        System.out.println("La expresión '" + expression2 + "' es " + (result2 ? "válida" : "inválida"));
+    }
+
 
 
 }
