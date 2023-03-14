927be0264dd43a9667e7d34676fbc750b1d22e67
Anaru03 <dele22428@uvg.edu.gt>
2023-03-14 16:43:21 -0600

    Desarrollo de Cond

--- src/Model/Cond.java
+++ src/Model/Cond.java
@@ -1,4 +1,31 @@
 package Model;
+import java.util.List;
+import java.util.function.Predicate;
+import java.lang.Object;
+
+/**
+ * Referencia de: http://arantxa.ii.uam.es/~rcobos/teaching/esp/ia/lisp0.pdf
+ */
 
 public class Cond extends AbstractFuncion{
+    public static Object cond(List<List<Object>> clauses) {
+        for (List<Object> clause : clauses) {
+            Object condition = clause.get(0);
+            Object result = clause.get(1);
+
+            if (condition == null) {
+                return result;
+            } else if (condition instanceof Boolean && (Boolean) condition) {
+                return result;
+            } else if (condition instanceof Predicate<?> && ((Predicate<?>) condition).test(null)) {
+                return result;
+            } else if (condition.equals("T")) {
+                return result;
+            }
+        }
+        return null;
+    }
+
+
+
 }
---
+++ src/Test/CondTest.java
@@ -0,0 +1,11 @@
+package Test;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CondTest {
+
+}
