1743a64624745600dcc134b10ae2bedfc127619c
Anaru03 <dele22428@uvg.edu.gt>
2023-03-14 17:52:15 -0600

    Intento del CondTest

--- src/Test/CondTest.java
+++ src/Test/CondTest.java
@@ -1,11 +1,24 @@
 package Test;
-import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.function.Supplier;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.Test;
+import Model.Cond;
 
 public class CondTest {
+
+    @Test
+    public void testCond() {
+        List<List<Object>> casos = new ArrayList<List<Object>>();
+
+        casos.add(Arrays.asList(">", 5, 3));
+        casos.add(Arrays.asList("=", 5, 3));
+        casos.add(Arrays.asList("T", "hello"));
+
+        Object result = Cond.cond(casos);
+        Assert.assertEquals("hello", result);
+    }
+
 
 }
