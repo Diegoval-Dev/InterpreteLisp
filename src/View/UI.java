763e4d0dc9169a367a4331fb12a1d39ccd021443
Anaru03 <dele22428@uvg.edu.gt>
2023-03-19 07:15:34 -0600

    Comentarios en View

--- src/View/UI.java
+++ src/View/UI.java
@@ -3,10 +3,17 @@ package View;
 import java.util.Scanner;
 
 public class UI {
+    /**
+     * @return regresa una cadena de texto correspondiente
+     */
     public String readCodigo(){
         Scanner sc = new Scanner(System.in);
         return sc.nextLine();
     }
+
+    /**
+     * @param m cadena de texto que recibe un mensaje como entrada y lo imprime en la consola
+     */
     public void print(String m){
         System.out.println(m);
     }
